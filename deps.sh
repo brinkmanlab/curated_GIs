34c26d8d53e5e133234da6475b737d4a8222abe9
Nolan Woods <nolan_w@sfu.ca>
2022-09-20 16:53:00 -0700

    Update setsimilaritysearch

--- deps.sh
+++ deps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 shopt -s expand_aliases failglob
 set -e
+
 export PROJECT=$(dirname $(realpath "$BASH_SOURCE"))
 wd=$(realpath --relative-to="$PROJECT" "$PWD")
 
@@ -9,6 +11,6 @@ alias bedtools="docker run --rm -i --user $(id -u):$(id -g) -v $PROJECT:/mnt -w 
 alias biopython.convert="$bpc"
 alias progressiveMauve="docker run --rm --user $(id -u):$(id -g) -v $PROJECT:/mnt -w /mnt/$wd quay.io/biocontainers/mauve:2.4.0.snapshot_2015_02_13--hdfd78af_4 progressiveMauve"
 alias blast="docker run --rm --user $(id -u):$(id -g) -v $PROJECT:/mnt -w /mnt/$wd quay.io/biocontainers/blast:2.12.0--pl5262h3289130_0"
-alias setsimilaritysearch="docker run --rm --user $(id -u):$(id -g) -v $PROJECT:/mnt -w /mnt/$wd quay.io/biocontainers/setsimilaritysearch:0.1.7 all_pairs.py"
+alias setsimilaritysearch="docker run --rm --user $(id -u):$(id -g) -v $PROJECT:/mnt -w /mnt/$wd quay.io/biocontainers/setsimilaritysearch:1.0.0 all_pairs.py"
 alias prokka="docker run --rm --user $(id -u):$(id -g) -v$PROJECT:/mnt -w /mnt/$wd staphb/prokka:latest prokka"
 alias diamond="docker run --rm --user $(id -u):$(id -g) -v$PROJECT:/mnt -w /mnt/$wd quay.io/biocontainers/diamond:2.0.15--hb97b32f_0 diamond"
